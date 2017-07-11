60ba9bab151331adf11eedb70717d493e099240a
Zach Mason <zacharycmason@gmail.com>
2017-07-11 16:38:30 -0700

    added person class to properties project

---
+++ Properties/Properties/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Properties
+{
+	public class Person
+	{
+		public DateTime Birthdate { get; set; }
+
+		public int Age
+		{
+			get
+			{
+				var timeSpan = DateTime.Today - Birthdate;
+				var years = timeSpan.Days / 365;
+				return years;
+			}
+		}
+	}
+}
---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var person = new Person();
+			person.Birthdate = new System.DateTime(1982, 1, 1);
+			System.Console.WriteLine(person.Age);
+		}
+	}
+}
