bae4512a575aefa20f75025628218a6174aad1d7
Zach Mason <zacharycmason@gmail.com>
2017-07-13 11:36:55 -0700

    added Inheritance project

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+	public class PresentationObject
+	{
+		public int Width { get; set; }
+		public int Height { get; set; }
+
+		public void Copy()
+		{
+			Console.WriteLine("Object copied to clipboard.");
+		}
+
+		public void Duplicate()
+		{
+			Console.WriteLine("Object copied to clipboard.");
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
