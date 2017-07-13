6d071a550e46c706b076942da88649df3e0d48a5
Zach Mason <zacharycmason@gmail.com>
2017-07-13 14:27:30 -0700

    customer class in sep file

---
+++ AccessModifiersTwo/AccessModifiersTwo/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AccessModifiersTwo
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public void Promote()
+		{
+			var rating = CalculateRating(excludeOrders: true);
+			if (rating == 0)
+				Console.WriteLine("Promoted to Level 1");
+			else
+				Console.WriteLine("Promoted to Level 2");
+		}
+
+		private int CalculateRating(bool excludeOrders)
+		{
+			return 0;
+		}
+	}
+}
--- AccessModifiersTwo/AccessModifiersTwo/Program.cs
+++ AccessModifiersTwo/AccessModifiersTwo/Program.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AccessModifiersTwo
 {
-	public class Customer
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-
-		public void Promote()
-		{
-
-		}
-	}
 	class Program
 	{
 		static void Main(string[] args)
