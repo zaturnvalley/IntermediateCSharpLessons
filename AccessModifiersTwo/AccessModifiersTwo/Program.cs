6b3663b6f11ec9a9ee942f843d3d5e47911ddbc8
Zach Mason <zacharycmason@gmail.com>
2017-07-13 14:01:51 -0700

    added accessmodifierstwo

---
+++ AccessModifiersTwo/AccessModifiersTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersTwo
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public void Promote()
+		{
+
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var customer = new Customer();
+		}
+	}
+}
