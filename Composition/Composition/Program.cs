190d407243e147740d5b1a8df15aa520a8673560
Zach Mason <zacharycmason@gmail.com>
2017-07-13 13:05:01 -0700

    fixed syntax error

--- Composition/Composition/Program.cs
+++ Composition/Composition/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,13 @@ namespace Composition
 	{
 		static void Main(string[] args)
 		{
+			var dbMigrator = new DbMigrator(new Logger());
+			var logger = new Logger();
+			var installer = new Installer(logger);
 
+			dbMigrator.Migrate();
+
+			installer.Install();
 		}
 	}
 }
