ea8cb3a8c91991324cc113f198163661d98d2dac
Zach Mason <zacharycmason@gmail.com>
2017-07-17 15:44:25 -0700

    examples of up and downcasting

--- Casting/Casting/Program.cs
+++ Casting/Casting/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,21 @@ namespace Casting
 	{
 		static void Main(string[] args)
 		{
+
+			// Upcasting
+			//Text text = new Text();
+			//Shape shape = text;
+			//text.Width = 100;
+			//shape.Width = 200;
+
+			//Or
+			//var List = new ArrayList();
+			//List.Add(1);
+			//List.Add(34);
+
+			// Downcasting
+			//Shape shape = new Text();
+			//Text text = (Text)shape;
 		}
 	}
 }
