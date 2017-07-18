c849600705fbf53ac2f76269702b8adcc2c95f82
Zach Mason <zacharycmason@gmail.com>
2017-07-18 09:29:27 -0700

    added boxing proj

---
+++ Boxing/Boxing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var list = new ArrayList();
+			list.Add(1);
+			list.Add("Zach");
+			list.Add(DateTime.Today);
+
+			var number = (int)list[1];
+
+		}
+	}
+}
