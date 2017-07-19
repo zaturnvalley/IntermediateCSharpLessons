b030ead2655bcd2f62182f0b06c5e1a1e29f0b91
Zach Mason <zacharycmason@gmail.com>
2017-07-19 15:22:45 -0700

    instance of triangle

--- MethodOverriding/MethodOverriding/Program.cs
+++ MethodOverriding/MethodOverriding/Program.cs
@@ -10,6 +10,12 @@ namespace MethodOverriding
 	{
 		static void Main(string[] args)
 		{
+			var shapes = new List<Shape>();
+			shapes.Add(new Circle());
+			shapes.Add(new Rectangle());
+
+			var canvas = new Canvas();
+			canvas.DrawShapes(shapes);
 		}
 	}
 }
--- MethodOverriding/MethodOverriding/Shape.cs
+++ MethodOverriding/MethodOverriding/Shape.cs
@@ -14,11 +14,20 @@ namespace MethodOverriding
 			Console.WriteLine("Draw a circle");
 		}
 	}
+
 	public class Rectangle : Shape
 	{
 		public override void Draw()
 		{
 			Console.WriteLine("Draw a rectangle");
+		}
+	}
+
+	public class Triangle : Shape
+	{
+		public override void Draw()
+		{
+			Console.WriteLine("Draw a triangle");
 		}
 	}
 	public class Shape
