8a05c5e03759b0387b102f87cae999d4e6d5453f
Zach Mason <zacharycmason@gmail.com>
2017-07-19 14:43:53 -0700

    added MethodOverriding Proj

---
+++ MethodOverriding/MethodOverriding/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+	public class Shape
+	{
+		public int Width { get; set; }
+		public int Height { get; set; }
+		public Position Position { get; set; }
+		public ShapeType Type { get; set; }
+	}
+}
