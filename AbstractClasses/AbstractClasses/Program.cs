df7cdcb642b009f2e3585a2f50de1daa9d0a089e
Zach Mason <zacharycmason@gmail.com>
2017-07-20 09:31:47 -0700

    added rectangle class

--- AbstractClasses/AbstractClasses/Program.cs
+++ AbstractClasses/AbstractClasses/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AbstractClasses
 {
+	public class Rectangle : Shape
+	{
+
+	}
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			var circle = new Circle();
+			circle.Draw();
+
+			var rectangle = new Rectangle();
+			rectangle.Draw();
 		}
 	}
 }
