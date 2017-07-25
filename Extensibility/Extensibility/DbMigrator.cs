454ec8ff8de398a58a4852dc42dd8fce95b4b081
Zach Mason <zacharycmason@gmail.com>
2017-07-25 11:17:21 -0700

    extensibility creates logs via stream

---
+++ Extensibility/Extensibility/DbMigrator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Extensibility
+{
+	public class DbMigrator
+	{
+		private readonly ILogger _logger;
+		// Dependency Injection
+		public DbMigrator(ILogger logger)
+		{
+			_logger = logger;
+		}
+		public void Migrate()
+		{
+			_logger.LogInfo("Migrating started at {0}" + DateTime.Now);
+
+			// Details of migrating the database
+
+			_logger.LogInfo("Migrating finished at {0}" + DateTime.Now);
+		}
+	}
+}
