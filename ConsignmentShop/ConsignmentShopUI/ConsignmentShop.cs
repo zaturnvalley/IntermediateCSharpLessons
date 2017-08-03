8b5205fe7b4df578fb1e3caaa11bd28770984e33
Zach Mason <zacharycmason@gmail.com>
2017-08-03 11:24:52 -0700

    populated items w dummy data

--- ConsignmentShop/ConsignmentShopUI/ConsignmentShop.cs
+++ ConsignmentShop/ConsignmentShopUI/ConsignmentShop.cs
@@ -22,27 +22,28 @@ namespace ConsignmentShopUI
 
 		private void SetupData()
 		{
-			//Vendor demoVendor = new Vendor();
-
-			//demoVendor.FirstName = "Bill";
-			//demoVendor.LastName = "Smith";
-			//demoVendor.Commission = .5;
-
-			//store.Vendors.Add(demoVendor);
-
-			//demoVendor = new Vendor();
-
-			//demoVendor.FirstName = "Sue";
-			//demoVendor.LastName = "Jones";
-			//demoVendor.Commission = .5;
-
-			//store.Vendors.Add(demoVendor);
-
-			//store.Vendors = new List<Vendor>();
-
+			//Dummy data
 			store.Vendors.Add(new Vendor { FirstName = "Bill", LastName = "Johnson"});
 			store.Vendors.Add(new Vendor { FirstName = "Sue", LastName = "Smith"});
 
+			//Populating Items
+			store.Items.Add(new Item {
+				Title ="War & Peace",
+				Description = @"Following the fortunes and the rise and 
+					fall of five aristocratic families in the waning days of imperial Russia.",
+				Price = 4.50M,
+				Owner = store.Vendors[0] });
+
+			store.Items.Add(new Item
+			{
+				Title = "East of Eden",
+				Description = @"Set in the rich farmland of California's Salinas Valley, 
+					this sprawling and often brutal novel follows the intertwined destinies 
+					of two families—the Trasks and the Hamiltons—whose generations helplessly 
+					reenact the fall of Adam and Eve and the poisonous rivalry of Cain and Abel.",
+				Price = 3.50M,
+				Owner = store.Vendors[1]
+			});
 		}
 
 		private void label1_Click(object sender, EventArgs e)
