e62c4b3f928687222a14f36bae63816cede25765
Zach Mason <zacharycmason@gmail.com>
2017-08-03 09:09:52 -0700

    added class library and vendor class

---
+++ ConsignmentShop/ConsignmentShopLibrary/Vendor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsignmentShopLibrary
+{
+	public class Vendor
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public double Commission { get; set; }
+	}
+}
